d45b3e2f61897ae908f928fb02498587bfd9a4b3
Raghu Srinivasan <raghu.srinivasan@gmail.com>
2020-11-26 13:35:50 -0800

    spellos fixed in comments

--- primorial_of_a_number.rb
+++ primorial_of_a_number.rb
@@ -3,7 +3,7 @@
 
 # we can avoid the math and difficulty of getting
 # primes or checking primes by usrng Ruby's built-in
-# prime generator to which we can pass how many pime #s we want
+# prime generator to which we can pass how many prime #s we want
 # we get back an array and it is trivial to find
 # the product of the elements of this array in 1 line
 
