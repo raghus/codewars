912ed337ccee9457278864a33701f53c8e1fe86e
Raghu Srinivasan <raghu.srinivasan@gmail.com>
2020-11-26 13:33:07 -0800

    Primorial of a Number

---
+++ primorial_of_a_number.rb
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5a99a03e4a6b34bb3c000124/train
+# 6 kyu
+
+# we can avoid the math and difficulty of getting
+# primes or checking primes by usirng Ruby's built-in
+# prime gemerator to which we can pass how many we want
+# we get back an array and it is trivial to find
+# the product of the elements of this array in 1 line
+
+
+require 'prime'
+def num_primorial(n)
+  (Prime.first n).inject(:*)
+end
