a57efa767860e77cc1cdfa757cd6bc9cdc182655
Raghu Srinivasan <raghu.srinivasan@gmail.com>
2020-12-08 21:11:50 -0800

    Perimeter of squares in a rectangle

--- perimeter_squares_in_rectangle.rb
+++ perimeter_squares_in_rectangle.rb
@@ -4,8 +4,8 @@
 def perimeter(n)
   # it's easy to see that the sides of the squares form a Fibonacci sequence
   # so all we need to do is to find the first n+1 Fib #s (ignoring the first as it is a 0)
-  # and then sum that array and multiple by 4 (4 sides for a square)
-  # first we popualte an array memo for the first n+1 Fib#s using meoization
+  # and then sum that array and multiply by 4 (as there are 4 sides for a square)
+  # first we populate an array memo for the first n+1 Fib#s using memo-ization
   # that's it. sum it up and quadruple it
   memo = []
   (0..n+1).each do |i|
